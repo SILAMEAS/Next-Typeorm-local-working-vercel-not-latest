bac891710f8ad98228ea90b19fbe1bb53dce9a4a
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2024-05-13 11:44:02 +0700

    done

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -6,10 +6,10 @@ import { Projects } from "@/constants";
 const Page = () => {
   return (
     <div
-      style={{ backgroundImage: "url(/mountains.jpg)" }}
+      style={{ backgroundImage: "url(/bg-3.jpg)" }}
       className="w-screen h-screen flex items-center justify-center bg-center bg-cover"
     >
-      <div className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]">
+      <div className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%] overflow-y-auto">
         {Projects.map((project, index) => (
           <ProjectCard
             key={project.src + project.url}
